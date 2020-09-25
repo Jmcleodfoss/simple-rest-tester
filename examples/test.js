8b76a4bddfe09a48755f9b1d7b12620631c754a9
James McLeod <jmcleodfoss@gmail.com>
2020-09-24 22:32:34 -0400

    Add template test script

---
+++ examples/test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const mocha_test = require('simple-rest-tester').mocha;
+
+function test(jsonFile)
+{
+	const reqDescr = require(jsonFile);
+
+	try {
+		mocha_test(reqDescr);
+	} catch (ex) {
+		console.log(ex);
+	}
+}
+
+// Order of tests is important if you are using macros ore relying on the existence of resources created by earlier tests in later tests
+test('./GET_ping_204.json')
+test('./POST_service_200.json')
+test('./POST_service_400.json')
+// etc
