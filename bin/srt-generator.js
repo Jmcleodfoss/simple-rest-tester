c2f71681bc2ef2e0827eb8f04d165a27e18ec46b
James McLeod <jmcleodfoss@gmail.com>
2020-12-21 06:53:08 -0500

    Don't include brace brackets in filenames

--- bin/srt-generator.js
+++ bin/srt-generator.js
@@ -138,7 +138,7 @@ function getPath(pathPrefix, path, srtObject)
 
 function getTestName(method, path, response)
 {
-	return method + '_' + path.split('/').slice(1).join('-') + '_' + response;
+	return (method + '_' + path.split('/').slice(1).join('-') + '_' + response).replace(/[{}]/g, '');
 }
 
 function getSchemaSRTValue(object, key, defaultValue)
